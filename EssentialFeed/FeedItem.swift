ee8408ad3d871f482d43d87232ba1b5b5fc45129
Trung Nguyen <tmnguyen@epic.com>
2023-08-26 10:10:21 -0500

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Trung Nguyen on 8/25/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
