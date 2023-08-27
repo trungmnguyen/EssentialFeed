dcca4d9ba7ea8276ee9f0deb021cea5b9ce0582c
Trung Nguyen <tmnguyen@epic.com>
2023-08-26 22:26:52 -0500

    Replace stubbing by capturing values instead

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -5,8 +5,8 @@
 //  Created by Trung Nguyen on 8/26/23.
 //
 
-import XCTest
 import EssentialFeed
+import XCTest
 
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDateFromURL() {
@@ -23,7 +23,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         XCTAssertEqual(client.requestedURLs, [url])
     }
-    
+
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
@@ -33,15 +33,17 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-    
+
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
+        
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
-        
     }
 
     // MARK: Helpers
@@ -54,12 +56,10 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
 
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
